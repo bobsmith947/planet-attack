f900c5691266964d3cbae33a99cf2dfc77c250d3
bobsmith947 <lucaskitaev@gmail.com>
2021-04-01 23:25:08 -0600

    Fix documentation links

--- app/src/main/java/edu/mines/csci448/planetattack/BackPressListener.kt
+++ app/src/main/java/edu/mines/csci448/planetattack/BackPressListener.kt
@@ -2,9 +2,9 @@ package edu.mines.csci448.planetattack
 
 /**
  * A simple interface to override the default behavior of the back button.
- * This is necessary because the Android <code>onBackPressed()</code> method is not available in fragments.
+ * This is necessary because [android.app.Activity.onBackPressed] is not available in fragments.
  * Fragments which implement this interface will be notified by the parent activity when the back button is pressed.
- * <a href="https://stackoverflow.com/a/46425415">See this Stack Overflow post for more information.</a>
+ * [See this Stack Overflow post for more information.](https://stackoverflow.com/a/46425415)
  */
 interface BackPressListener {
 	fun onBackPressed()
